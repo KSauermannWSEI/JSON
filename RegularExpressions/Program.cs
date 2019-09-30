2118091780a6063ca1cb55130b7c216a8e7a5274
KSauermannWSEI <krzysztof.sauermann@outlook.com>
2019-09-30 17:10:34 +0200

    OK

--- RegularExpressions/Program.cs
+++ RegularExpressions/Program.cs
@@ -13,6 +13,7 @@ namespace RegularExpressions
         {
             while (true)
             {
+                //Regex regex = new Regex(@"ok|OK");
                 Regex regex = new Regex(@"^((?:(?:[a-zA-Z0-9_\-\.]+)@(?:(?:\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(?:(?:[a-zA-Z0-9\-]+\.)+))(?:[a-zA-Z]{2,4}|[0-9]{1,3})(?:\]?)(?:\s*;\s*|\s*$))*)$");
                 string text = Console.ReadLine();
                 Match match = regex.Match(text);
